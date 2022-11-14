3e47d1f74c30a3b2de2430bbdec527cb2b73689a
TaChyla <tachylad.murray@gmail.com>
2022-11-14 12:58:36 -0500

    returns null when city is not provided

--- test/bikesAPIproxy.test.js
+++ test/bikesAPIproxy.test.js
@@ -37,4 +37,40 @@ describe("citibikes API", () => {
        // ASSERTION
        expect(result).toBe("/v2/networks/cogo");
     });
+
+    it('returns null if a city is not provided', async () => {
+                     // ARRANGE
+                     const data = {
+                        "networks": [
+                            {
+                                "href": "/v2/networks/fake",
+                                "location": {
+                                    "city": "Columbus",
+                                    "country": "fakeCountry",
+                                },
+                            },
+                            {
+                                "href": "/v2/networks/fakePath",
+                                "location": {
+                                    "city": "Miami, FL",
+                                    "country": "US",
+                                },
+                            },
+                            {
+                                "href": "/v2/networks/cogo",
+                                "location": {
+                                    "city": "Columbus, OH",
+                                    "country": "US",
+                                }
+                            }
+                        ]
+                    };
+                    axios.get.mockResolvedValueOnce({"data": data});
+
+                    //ACTION
+                    const result = await getNetworkHrefByCity(null);
+
+                    //ASSERTION
+                    expect(result).toBe(null);
+            });
 });
