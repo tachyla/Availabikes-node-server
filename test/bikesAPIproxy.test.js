1520b16387da9c0558e7483a40bc23229547728b
TaChyla <tachylad.murray@gmail.com>
2022-12-19 18:54:29 -0500

    getStation requires both N number of bikes & Href

--- test/bikesAPIproxy.test.js
+++ test/bikesAPIproxy.test.js
@@ -72,7 +72,7 @@ describe("citibikes API", () => {
 
                     //ASSERTION
                     expect(result).toBe(null);
-            });
+    });
 
     it('returns null if a city does not exist', async () => {
                         // ARRANGE
@@ -107,16 +107,14 @@ describe("citibikes API", () => {
 
                         // ASSERT
                         expect(result).toBe(null);
-                    });
+    });
 });
 
 describe("get network stations by href", () => {
-    afterEach(() => {
-        jest.resetAllMocks()
-    });
+    let data;
 
-    it('returns null when no href is provided', async () => {
-        const data = {
+    beforeEach(() => {
+        data = {
             "stations": [
                 {
                     "empty_slots": 9,
@@ -130,28 +128,31 @@ describe("get network stations by href", () => {
                 }
             ]
         };
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks()
+    });
+
+    it('returns null when no href is provided', async () => {
         axios.get.mockResolvedValueOnce({"data": data});
 
        const result = await getNetworkStationsByHref(null);
         expect(result).toBe(null);
     });
 
+    it('returns null when number of bikes is not provided', async () => {
+         // ARRANGE
+         axios.get.mockResolvedValueOnce({"data": data});
+         let numberOfBikes = 5;
+ 
+         const result = await getNetworkStationsByHref("/v2/networks/cincy-red-bike");
+
+         expect(result).toBe(null);
+    });
+
     it("returns first station name that has at least N number of bikes", async () => {
-        // ARRANGE
-        const data = {
-            "stations": [
-                {
-                    "empty_slots": 9,
-                    "free_bikes": 2,
-                    "name": "City Hall",
-                },
-                {
-                    "empty_slots": 2,
-                    "free_bikes": 13,
-                    "name": "High St & Warren"
-                }
-            ]
-        };
+
         axios.get.mockResolvedValueOnce({"data": data});
         let numberOfBikes = 3;
 
@@ -161,5 +162,6 @@ describe("get network stations by href", () => {
         // ASSERTION
         expect(result).toBe("High St & Warren");
     });
-    it("returns list of 10> stations that have at least N number of bikes", async () => {})
+
+    it("returns list of 10> stations that have at least N number of bikes", async () => {});
 });
